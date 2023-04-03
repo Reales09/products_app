d0e8cccad59147c02d249db09d661f988f9c3e0c
Reales Myles <rclarence123@gmail.com>
2023-04-02 23:41:35 -0500

    Se configura formulario para login

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -19,13 +19,51 @@ class LoginScreen extends StatelessWidget {
                   SizedBox(height: 10),
                   Text('Login', style: Theme.of(context).textTheme.headline4),
                   SizedBox(height: 30),
-                  Text('Formulario'),
+                  // Text('Formulario'),
+                  _LoginForm(),
                 ],
               ),
+            ),
+            SizedBox(height: 50),
+            Text(
+              'Crear nueva cuenta',
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
             ),
           ]),
         ),
       ),
     );
+  }
+}
+
+class _LoginForm extends StatelessWidget {
+  const _LoginForm({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        child: Form(
+      //TODO: Manter la referencia al KEY
+      child: Column(
+        children: [
+          TextFormField(
+            autocorrect: false,
+            keyboardType: TextInputType.emailAddress,
+            decoration: InputDecoration(
+              enabledBorder: UnderlineInputBorder(
+                borderSide: BorderSide(color: Colors.deepPurple),
+              ),
+              focusedBorder: UnderlineInputBorder(
+                borderSide: BorderSide(color: Colors.deepPurple, width: 2),
+              ),
+              hintText: 'Usuario',
+              labelText: 'Usuario',
+              labelStyle: TextStyle(color: Colors.grey),
+              prefixIcon: Icon(Icons.person_outline, color: Colors.deepPurple),
+            ),
+          ),
+        ],
+      ),
+    ));
   }
 }
