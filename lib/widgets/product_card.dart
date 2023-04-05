00e195b538960e7f35f18d80161eb62884d72dd8
Reales Myles <rclarence123@gmail.com>
2023-04-05 09:49:27 -0500

    Se agrega tags para el product card

--- lib/widgets/product_card.dart
+++ lib/widgets/product_card.dart
@@ -17,6 +17,10 @@ class ProductCard extends StatelessWidget {
           children: [
             _BackgroundImage(),
             _ProductDetails(),
+            Positioned(top: 0, right: 0, child: _PriceTag()),
+
+            //TODO mostrar de manera condicional
+            Positioned(top: 0, left: 0, child: _NotAvaliable()),
           ],
         ),
       ),
@@ -36,6 +40,66 @@ class ProductCard extends StatelessWidget {
       );
 }
 
+class _NotAvaliable extends StatelessWidget {
+  const _NotAvaliable({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: FittedBox(
+        fit: BoxFit.contain,
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 10),
+          child: Text('No disponible',
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold)),
+        ),
+      ),
+      width: 100,
+      height: 70,
+      alignment: Alignment.center,
+      decoration: BoxDecoration(
+          color: Colors.red,
+          borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(25), bottomRight: Radius.circular(25))),
+    );
+  }
+}
+
+class _PriceTag extends StatelessWidget {
+  const _PriceTag({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: FittedBox(
+        fit: BoxFit.contain,
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 10),
+          child: Text('\$ 150.00',
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold)),
+        ),
+      ),
+      width: 100,
+      height: 80,
+      alignment: Alignment.center,
+      decoration: BoxDecoration(
+          color: Colors.black54,
+          borderRadius: BorderRadius.only(
+              topRight: Radius.circular(25), bottomLeft: Radius.circular(25))),
+    );
+  }
+}
+
 class _ProductDetails extends StatelessWidget {
   const _ProductDetails({
     super.key,
@@ -43,12 +107,43 @@ class _ProductDetails extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      height: 70,
-      color: Colors.red,
+    return Padding(
+      padding: const EdgeInsets.only(right: 50),
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+        height: 70,
+        decoration: _buildBoxDecoration(),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Disco duro SSD 1TB',
+              style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold),
+              maxLines: 1,
+              overflow: TextOverflow.ellipsis,
+            ),
+            Text(
+              'id del disco duro',
+              style: TextStyle(
+                fontSize: 15,
+                color: Colors.white,
+              ),
+              maxLines: 1,
+              overflow: TextOverflow.ellipsis,
+            ),
+          ],
+        ),
+      ),
     );
   }
+
+  BoxDecoration _buildBoxDecoration() => BoxDecoration(
+      color: Colors.black,
+      borderRadius: BorderRadius.only(
+          bottomLeft: Radius.circular(25), topRight: Radius.circular(25)));
 }
 
 class _BackgroundImage extends StatelessWidget {
