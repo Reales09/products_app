5afc243ad2c27314efec3c11773aec4a8b1cb7ea
Reales Myles <rclarence123@gmail.com>
2023-04-10 00:37:12 -0500

    Se agrega secure storage

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -1,11 +1,13 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:http/http.dart' as http;
 
 class AuthService extends ChangeNotifier {
   final String _baseUrl = 'identitytoolkit.googleapis.com';
   final String _firebaseToken = 'AIzaSyDVpjQSFEE-TK59N4draOmJGho2xMfOkF4';
+  final storage = FlutterSecureStorage();
 
 // Si retornamos algo, es un error, si no, todo bien
   Future<String?> createUser(String email, String password) async {
@@ -26,6 +28,8 @@ class AuthService extends ChangeNotifier {
 
     if (decodedResp.containsKey('idToken')) {
       // token hay que guardarlo en un lugar seguro
+
+      await storage.write(key: 'token', value: decodedResp['idToken']);
 
       // return decodedResp['idToken'];
 
@@ -54,9 +58,17 @@ class AuthService extends ChangeNotifier {
 
       // return decodedResp['idToken'];
 
+      await storage.write(key: 'token', value: decodedResp['idToken']);
+
       return null;
     } else {
       return decodedResp['error']['message'];
     }
+  }
+
+  Future logout() async {
+    await storage.delete(key: 'token');
+
+    return;
   }
 }
